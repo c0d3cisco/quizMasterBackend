a45e315cc31e273e1433a25e86dbbca4dbb77a30
Francisco Sanchez <c0d3cisco@yahoo.com>
2023-06-02 18:57:40 -0500

    inital commit

---
+++ client/index.js
@@ -0,0 +1,23 @@
+'use strict';
+// VENDOR //
+const handleOrder = require('./handler.js');
+const Chance = require('chance');
+const { io } = require('socket.io-client');
+const socket = io('http://localhost:3001/math');
+
+
+socket.on('connect', () => console.log('Vendor Link with Server: SUCCESSFUL'));
+
+socket.on('validationTest', (payload) =>{
+  console.log(payload);
+} );
+
+
+
+process.stdin.on('data', data => {
+  if(data.toString().slice(0, -1) === 'hi'){
+    socket.emit('validationTest', 'hello' );
+  }
+});
+
+
---
+++ sever/index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+// SERVER //
+require('dotenv').config({ path: '../.env'});
+const { Server } = require('socket.io');
+const Queue = require('./queue.js');
+const PORT = process.env.PORT || 3002;
+
+// combinations/initiations
+const io = new Server();
+const math = io.of('/math');
+
+
+//create connection point for endpoint
+math.on('connection', (socket) => {
+
+  console.log('sockets connected to cap namespace', socket.id);
+  socket.on('validationTest', (payload) =>{
+    socket.emit('respond', 'back to you');
+  } );
+
+
+
+
+  
+});
+
+
+
+io.listen(PORT);
